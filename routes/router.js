01660e6ca86bd3b3b5fbd005b77489f069c729ef
Christopher Egbaaibon <christopheregbaaibon@gmail.com>
2022-02-18 00:06:32 +0100

    another try

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,12 @@
-// Routes 
-const router   = require('express').Router();
-const { AddEmail, GetEmails } = require('../controller.js/handler');
+// Routes
+const router = require("express").Router();
+const { AddEmail, GetEmails } = require("../controller.js/handler");
 
-router.use((req, res, next)=>{
-   console.log('Good to go!')
-   next()
+router.use((req, res, next) => {
+  console.log("Good to go!");
+  next();
 });
 
-
-router.post('/addEmail', AddEmail);
-router.get('/emails', GetEmails);
+router.post("/addEmail", AddEmail);
+router.get("/emails", GetEmails);
 module.exports = router;
