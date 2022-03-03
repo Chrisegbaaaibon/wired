21cab157ca2480d53579009799cefbdaed45b9ed
Christopher Egbaaibon <christopheregbaaibon@gmail.com>
2022-03-03 21:04:09 +0100

    timestamp-update

--- database/schema.js
+++ database/schema.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const timestamps = require('mongoose-timestamp')
+
+
 
 const bear = new  Schema ({
    email : {
       type: String,
       required: true,
       unique: true
+   },
+},
+{
+      timestamps: true
    }
-});
+   );
 
 module.exports = mongoose.model("bear", bear);
